15745e04e34de05959b33480866f5cb857105fc9
Ahmad Moussawi <aymoussawi@gmail.com>
2018-01-21 11:59:06 +0200

    add Models project

---
+++ Sqlkata.Models/Model.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using SqlKata.Compilers;
+using SqlKata.Execution;
+
+namespace Sqlkata.Models
+{
+    public class Model<T> where T : Model<T>
+    {
+        private XQuery query;
+        protected bool touched;
+        protected string tableName;
+        protected string idColumn = "id";
+
+        protected Dictionary<string, object> currentAttributes = new Dictionary<string, object>();
+        protected Dictionary<string, object> newAttributes = new Dictionary<string, object>();
+
+        public Model(IDbConnection connection, Compiler compiler)
+        {
+            this.query = new XQuery(connection, compiler).From(tableName) as XQuery;
+        }
+
+        public T Find(object id)
+        {
+            return query.Where(idColumn, id).FirstOrDefault<T>();
+        }
+
+        public T Save()
+        {
+
+            if (touched)
+            {
+                query.Where(idColumn, this.currentAttributes[idColumn]).Update(newAttributes);
+            }
+            else
+            {
+                this.query.Insert(newAttributes);
+            }
+
+            return this as T;
+
+        }
+
+        public int Delete()
+        {
+            return this.query.Where(idColumn, this.currentAttributes[idColumn]).Delete();
+        }
+
+
+
+    }
+}
