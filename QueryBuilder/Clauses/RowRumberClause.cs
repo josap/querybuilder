43b092670a3dbc9a4afe58f568179c5f96610a8c
josap <38653194+josap@users.noreply.github.com>
2019-01-10 21:14:44 -0200

    NewFile

---
+++ QueryBuilder/Clauses/RowRumberClause.cs
@@ -0,0 +1,23 @@
+﻿namespace SqlKata {
+    public abstract class AbstractRowRumber : AbstractClause
+    {
+
+    }
+
+    //New 2019-01-10
+    public class RowRumber : AbstractRowRumber
+    {
+        public string Column { get; set; }
+
+        /// <inheritdoc />
+        public override AbstractClause Clone()
+        {
+            return new RowRumber {
+                Engine = Engine,
+                Component = Component,
+                Column = Column,
+            };
+        }
+    }
+
+}
